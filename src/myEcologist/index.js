303cbefd5d638026c2eb82075a36863b9efc90c7
kolhe-apurva <apurvakolhe@outlook.com>
2023-10-27 09:42:09 -0400

    updating with carousel content

--- src/myEcologist/index.js
+++ src/myEcologist/index.js
@@ -4,18 +4,22 @@ import Header from "./Header";
 import Home from "./Home/Home";
 import Footer from "./Footer";
 import Login from "./Login/Login";
+import HomeMainCarousel from "./Home/Carousel/Home/Carousel";
+import HomeCommunityCarousel from "./Home/CommunityCarousel/CommunityCarousel";
 
 function Ecologist() {
-    return (
-        <BrowserRouter>
-            <Header />
-            <Routes>
-                <Route path="/home" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-            </Routes>
-            <Footer />
-        </ BrowserRouter>
-    );
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+      <HomeMainCarousel />
+      <HomeCommunityCarousel />
+      <Footer />
+    </BrowserRouter>
+  );
 }
 
 export default Ecologist;
