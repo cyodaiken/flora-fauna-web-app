22179ca864d2d4f92630be3fad9a84cedab5027a
wjdeogus03 <chung.daeh@northeastern.edu>
2023-12-01 13:30:50 -0800

    add api

---
+++ src/myEcologist/Explore/client.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+const EXPLORE_URL = "http://localhost:400/project/explore";
+
+export const fetchExplores = async () => {
+    const response = await axios.get(`${EXPLORE_URL}`);
+    return response.data;
+};
+
+export const fetchExplore = async (id) => {
+    const response = await axios.get(`${EXPLORE_URL}/${id}`);
+    return response.data;
+}
+
+export const addNewExplore = async (explore) => {
+    const response = await axios.post(`${EXPLORE_URL}`, explore);
+    return response.data;
+}
+
+export const deleteExplore = async (id) => {
+    const response = await axios.delete(`${EXPLORE_URL}/${id}`);
+    return response.data;
+}
+
+export const updateExplore = async (id, explore) => {    
+    const response = await axios.put(`${EXPLORE_URL}/${id}`, explore);
+    return response.data;
+}
