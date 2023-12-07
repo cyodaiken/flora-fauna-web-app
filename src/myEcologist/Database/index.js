f10c0beb73dc54ffc3512643adca56001bc2846b
jaebutler <bossjae16@gmail.com>
2023-12-07 09:27:08 -0800

    delete database

--- src/myEcologist/Database/index.js
+++
@@ -1,9 +0,0 @@
-import species from './species.json';
-import observations from './observations.json';
-import users from './users.json';
-
-export default {
-    species,
-    observations,
-    users,
-};
